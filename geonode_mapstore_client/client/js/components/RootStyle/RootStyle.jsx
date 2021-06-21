32236fc07b29d2791a254c150ab5987e14bdcfbb
allyoucanmap <stefano.bovio@geo-solutions.it>
2021-06-21 11:48:54 +0200

    WIP

--- geonode_mapstore_client/client/js/components/RootStyle/RootStyle.jsx
+++ geonode_mapstore_client/client/js/components/RootStyle/RootStyle.jsx
@@ -19,6 +19,55 @@ function rootVariables(variables) {
     '\n}';
 }
 
+function buttonTheme(style, name) {
+    const prefix = name ? '-' + name : '';
+    const bg = style.bg;
+    const color = style.color || tinycolor.mostReadable(bg, ['#ffffff', '#000000'], {
+        includeFallbackColors: true
+    }).toHexString();
+
+    const borderColor = style.borderColor || tinycolor(bg).darken(8).toString();
+    const focusColor = style.focusColor || color;
+    const focusBg = style.focusBg || tinycolor(bg).darken(10).toString();
+    const focusBorderColor = style.focusBorderColor || tinycolor(bg).darken(25).toString();
+    const hoverColor = style.hoverColor || color;
+    const hoverBg = style.hoverBg || tinycolor(bg).darken(10).toString();
+    const hoverBorderColor = style.hoverBorderColor || tinycolor(bg).darken(12).toString();
+    const activeColor = style.activeColor || color;
+    const activeBg = style.activeBg || tinycolor(bg).darken(10).toString();
+    const activeBorderColor = style.activeBorderColor || tinycolor(bg).darken(12).toString();
+    const activeHoverColor = style.activeHoverColor || color;
+    const activeHoverBg = style.activeHoverBg || tinycolor(bg).darken(17).toString();
+    const activeHoverBorderColor = style.activeHoverBorderColor || tinycolor(bg).darken(25).toString();
+    const disableColor = style.disableColor || color;
+    const disabledBg = style.disabledBg || tinycolor(bg).desaturate(10).toString();
+    const disabledBorderColor = style.disabledBorderColor || tinycolor(bg).desaturate(50).toString();
+    const badgeColor = style.badgeColor || bg;
+    const badgeBg = style.badgeBg || color;
+    return {
+        [`--gn-button${prefix}-color`]: color,
+        [`--gn-button${prefix}-bg`]: bg,
+        [`--gn-button${prefix}-border-color`]: borderColor,
+        [`--gn-button${prefix}-focus-color`]: focusColor,
+        [`--gn-button${prefix}-focus-bg`]: focusBg,
+        [`--gn-button${prefix}-focus-border-color`]: focusBorderColor,
+        [`--gn-button${prefix}-hover-color`]: hoverColor,
+        [`--gn-button${prefix}-hover-bg`]: hoverBg,
+        [`--gn-button${prefix}-hover-border-color`]: hoverBorderColor,
+        [`--gn-button${prefix}-active-color`]: activeColor,
+        [`--gn-button${prefix}-active-bg`]: activeBg,
+        [`--gn-button${prefix}-active-border-color`]: activeBorderColor,
+        [`--gn-button${prefix}-active-hover-color`]: activeHoverColor,
+        [`--gn-button${prefix}-active-hover-bg`]: activeHoverBg,
+        [`--gn-button${prefix}-active-hover-border-color`]: activeHoverBorderColor,
+        [`--gn-button${prefix}-disabled-color`]: disableColor,
+        [`--gn-button${prefix}-disabled-bg`]: disabledBg,
+        [`--gn-button${prefix}-disabled-border-color`]: disabledBorderColor,
+        [`--gn-button${prefix}-badge-color`]: badgeColor,
+        [`--gn-button${prefix}-badge-bg`]: badgeBg
+    };
+}
+
 function RootStyle({
     theme
 }) {
@@ -39,7 +88,9 @@ function RootStyle({
                 focus,
                 tag,
                 badge,
-                footer
+                footer,
+                buttonDefault,
+                buttonPrimary
             } = theme;
             const color = themeColor && tinycolor.mostReadable(themeColor, ['#ffffff', '#000000'], {
                 includeFallbackColors: true
@@ -67,7 +118,7 @@ function RootStyle({
                     '--gn-disabled-bg': disabled.bg || '#fcfcfc'
                 }),
                 ...(danger && {
-                    '--gn-danger-color': danger.color || '#D0021B'
+                    '--gn-danger': danger || '#D0021B'
                 }),
                 ...(primary && {
                     '--gn-primary-color': primary.color || '#ffffff',
@@ -106,7 +157,12 @@ function RootStyle({
                         '--gn-footer-link-color': footer.link.color || '#397AAB',
                         '--gn-footer-link-hover-color': footer.link.hoverColor || '#1b4d74'
                     })
-                })
+                }),
+                ...buttonDefault?.bg && buttonTheme(buttonDefault),
+                ...(buttonPrimary?.bg || themeColor || primary?.bg) && buttonTheme({
+                    bg: themeColor || primary?.bg,
+                    ...buttonPrimary
+                }, 'primary')
             };
         }
         return {};
